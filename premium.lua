77796459270ab6bd0a893fe416d544a14e279bfe
deephubpro <kietrobloxfruit@gmail.com>
2025-03-05 03:56:50 +0700

    Create premium.lua

---
+++ premium.lua
@@ -0,0 +1,5 @@
+if game.GameId == 994732206 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/deephubpro/premium/refs/heads/main/Bloxfruit.lua"))()
+elseif game.GameId == 6401952734 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/deephubpro/Deephub/refs/heads/main/bkshoajvt.lua"))()
+end
